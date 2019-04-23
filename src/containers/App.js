c7a801a6b1cccae399a35fb190b1af2b79852d17
Anthony Ho <anthony.ho@thehutgroup.com>
2019-04-23 15:30:08 +0100

    React lazy

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,29 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.scss';
-import Home from './Home/Home';
-import Portfolio from './Portfolio/Portfolio';
-import Photography from './ImageGallery/ImageGallery';
-import Navheader from '../components/Navheader/Navheader';
 import Footer from '../components/footer/footer';
 import {Route, Switch} from 'react-router-dom';
 import {HashRouter} from 'react-router-dom';
 
+const Navheader = React.lazy(() => import ('../components/Navheader/Navheader'));
+const Home = React.lazy(() => import ('./Home/Home'));
+const Portfolio = React.lazy(() => import ('./Portfolio/Portfolio'));
+const Photography = React.lazy(() => import ('./ImageGallery/ImageGallery'));
+
 class App extends Component {
-   render() {
-    return (
-      <HashRouter>
-        <div className="appContainer">
-          <Navheader/>
-            <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/portfolio" component={Portfolio}/>
-            <Route path="/photography" component={Photography}/>
-            </Switch>
-            <Footer/>
-            </div>
-      </HashRouter>
-    )
-  }
+    render() {
+        return (
+            <HashRouter>
+                <div className="appContainer">
+                    <React.Suspense fallback={< div > Loading nav ...</div>}>
+                        <Navheader/>
+                    </React.Suspense>
+                    <React.Suspense fallback={< div > Loading page ...</div>}>
+                        <Switch>
+
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/portfolio" component={Portfolio}/>
+                            <Route path="/photography" component={Photography}/>
+
+                        </Switch>
+                    </React.Suspense>
+                    <Footer/>
+                </div>
+            </HashRouter>
+        )
+    }
 }
 
 export default App;
