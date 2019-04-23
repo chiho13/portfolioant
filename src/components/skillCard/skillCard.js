c281438be1052afd38d538ded519451efd7698b0
Anthony Ho <anthony.ho@thehutgroup.com>
2019-04-23 17:59:41 +0100

    updated webpack

--- src/components/skillCard/skillCard.js
+++ src/components/skillCard/skillCard.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './skillCard.scss';
 
-
-
 export default class SkillCard extends Component {
 	render() {
       return (
         <div className="skillCard">
             <div className="skillCard_container">
-              <a className="skillCard_cvlink" target="_blank" href="https://docs.google.com/document/d/1QAIibxdjqjbHYJSrInXRD9wu72CKjASkh6Fb9ys_GAQ/edit?usp=sharing">
+              <a className="skillCard_cvlink" target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1QAIibxdjqjbHYJSrInXRD9wu72CKjASkh6Fb9ys_GAQ/edit?usp=sharing">
                 Open CV
               </a>
             </div>
